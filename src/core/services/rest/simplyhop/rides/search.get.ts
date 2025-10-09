98335c9cf700461a52611e82a0f9ddf950309d25
milyasbpa <basilyas.2015@gmail.com>
2025-10-09 10:20:45 +0700

    fix: get rides must login

--- src/core/services/rest/simplyhop/rides/search.get.ts
+++ src/core/services/rest/simplyhop/rides/search.get.ts
@@ -3,6 +3,7 @@ import { ENVIRONMENTS } from "@/core/environments";
 import { SimplyHopAPICollectionURL } from "@/core/utils/router/constants/simplyhop_api";
 import { GetRidesSearchPayloadRequestInterface } from "@/core/models/rest/simplyhop/rides";
 import axios from "@/core/utils/axios/functions/base";
+import Cookies from "universal-cookie";
 
 export const fetchGetRidesSearch = async (
   payload?: GetRidesSearchPayloadRequestInterface
@@ -12,7 +13,14 @@ export const fetchGetRidesSearch = async (
       ENVIRONMENTS.SIMPLY_HOP_API_URL
     }${SimplyHopAPICollectionURL.rides.getSearch()}`;
 
-    const res = await axios.get(url, { params: payload?.params });
+    const cookies = new Cookies();
+    const token = cookies.get("token");
+    const res = await axios.get(url, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+      params: payload?.params,
+    });
     return res.data;
   } catch (err) {
     throw (err as AxiosError)?.response?.data || (err as AxiosError)?.response;
