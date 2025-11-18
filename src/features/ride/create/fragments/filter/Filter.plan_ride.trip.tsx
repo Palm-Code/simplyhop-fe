f49ba9912c4225fe8261546b9b4d35cef23bdcf9
milyasbpa <basilyas.2015@gmail.com>
2025-11-19 00:59:02 +0700

    fix: gap

--- src/features/ride/create/fragments/filter/Filter.plan_ride.trip.tsx
+++ src/features/ride/create/fragments/filter/Filter.plan_ride.trip.tsx
@@ -438,7 +438,7 @@ export const FilterPlanRideTrip = () => {
   return (
     <div
       className={clsx(
-        "grid grid-cols-1 place-content-start place-items-start gap-[1.5rem] sm:gap-[2rem]",
+        "grid grid-cols-1 place-content-start place-items-start gap-[1.5rem] sm:gap-[1rem]",
         "w-[100vw] lg:w-[calc(100vw-2rem)] container:w-full container:max-w-container",
         "px-[1rem] py-[1rem] sm:px-[1.5rem] sm:py-[1.5rem]",
         "bg-[#FFFFFFCC] dark:bg-[#232323CC]",
