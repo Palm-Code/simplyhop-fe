e5fa46bdb2bcde12081ce930638a413db0ccffa3
milyasbpa <basilyas.2015@gmail.com>
2025-11-25 11:26:38 +0700

    fix: spacing

--- src/app/(protected)/(dashboard)/support/fahrten/page.tsx
+++ src/app/(protected)/(dashboard)/support/fahrten/page.tsx
@@ -7,33 +7,41 @@ import { formatDisplayName } from "@/core/utils/name/functions";
 import { ListTripContainer } from "@/features/trip/list/container";
 import { ListTripProvider } from "@/features/trip/list/context";
 import { useContext } from "react";
+import clsx from "clsx";
 
 export default function TripPage() {
   const { state: userState } = useContext(UserContext);
   return (
     <DashboardLayout>
-      <DashboardHeader
-        title={"Fahrten"}
-        avatar={{
-          src: userState.profile?.avatar,
-        }}
-        name={formatDisplayName({
-          first_name: userState.profile?.first_name,
-          email: userState.profile?.email,
-        })}
-        role={
-          userState.profile?.role === "admin" &&
-          userState.profile.is_super_admin
-            ? "Super Admin"
-            : userState.profile?.role === "admin"
-            ? "Admin"
-            : "Employee"
-        }
-      />
-      <ListTripProvider>
-        <ListTripContainer />
-        <AlertApp />
-      </ListTripProvider>
+      <div
+        className={clsx(
+          "grid grid-cols-1 place-content-start place-items-start gap-[1.5rem]",
+          "w-full"
+        )}
+      >
+        <DashboardHeader
+          title={"Fahrten"}
+          avatar={{
+            src: userState.profile?.avatar,
+          }}
+          name={formatDisplayName({
+            first_name: userState.profile?.first_name,
+            email: userState.profile?.email,
+          })}
+          role={
+            userState.profile?.role === "admin" &&
+            userState.profile.is_super_admin
+              ? "Super Admin"
+              : userState.profile?.role === "admin"
+              ? "Admin"
+              : "Employee"
+          }
+        />
+        <ListTripProvider>
+          <ListTripContainer />
+          <AlertApp />
+        </ListTripProvider>
+      </div>
     </DashboardLayout>
   );
 }
