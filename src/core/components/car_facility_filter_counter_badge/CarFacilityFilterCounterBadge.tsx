ae4942ec80ae5cbd6cf51edddc4ddbe22ee8c453
milyasbpa <basilyas.2015@gmail.com>
2025-11-26 13:45:23 +0700

    fix: car facility filter

--- src/core/components/car_facility_filter_counter_badge/CarFacilityFilterCounterBadge.tsx
+++ src/core/components/car_facility_filter_counter_badge/CarFacilityFilterCounterBadge.tsx
@@ -22,8 +22,12 @@ export const CarFacilityFilterCounterBadge = ({
       className={clsx(
         "grid grid-flow-col place-content-center place-items-center gap-[0.5rem]",
         "px-[0.5rem] py-[0.25rem]",
-        "bg-[#FAFFF9] dark:bg-[#232323]",
-        "border border-[#33CC33] dark:border-[#464646]",
+        !!count
+          ? "bg-[#FAFFF9] dark:bg-[#232323]"
+          : "bg-white dark:bg-[#232323]",
+        !!count
+          ? "border border-[#33CC33] dark:border-[#464646]"
+          : "border border-[#E9E6E6] dark:border-[#464646]",
         "rounded-[1.25rem]",
         "cursor-pointer"
       )}
@@ -31,23 +35,37 @@ export const CarFacilityFilterCounterBadge = ({
     >
       <SVGIcon
         name={icon}
-        className={clsx("w-[0.75rem] h-[0.75rem]", "text-[#33CC33] dark:text-[#F6F6F6]")}
+        className={clsx(
+          "w-[0.75rem] h-[0.75rem]",
+          !!count
+            ? "text-[#33CC33] dark:text-[#E9E6E6]"
+            : "text-[#636262] dark:text-[#E9E6E6]"
+        )}
       />
-      <span className={clsx("text-[#33CC33] dark:text-[#E9E6E6] text-[0.875rem] font-medium")}>
+      <span
+        className={clsx(
+          !!count
+            ? "text-[#33CC33] dark:text-[#E9E6E6]"
+            : "text-[#636262] dark:text-[#E9E6E6]",
+          "text-[0.875rem] font-medium"
+        )}
+      >
         {label}
       </span>
 
-      <div
-        className={clsx(
-          "flex items-center justify-center",
-          "w-[1.25rem] h-[1.25rem]",
-          "bg-[#33CC33]",
-          "rounded-[50%]",
-          "text-[white] text-[0.625rem] font-medium"
-        )}
-      >
-        {count}
-      </div>
+      {!!count && (
+        <div
+          className={clsx(
+            "flex items-center justify-center",
+            "w-[1.25rem] h-[1.25rem]",
+            "bg-[#33CC33]",
+            "rounded-[50%]",
+            "text-[white] text-[0.625rem] font-medium"
+          )}
+        >
+          {count}
+        </div>
+      )}
     </button>
   );
 };
