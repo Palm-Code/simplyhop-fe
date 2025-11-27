fcc87d1f04a3b70fb23569f14a0feaf9c1d67f72
milyasbpa <basilyas.2015@gmail.com>
2025-11-27 08:48:16 +0700

    fix: formatUnreadMessageNumber

--- src/core/utils/chat/functions/formatUnreadMessageNumber.ts
+++ src/core/utils/chat/functions/formatUnreadMessageNumber.ts
@@ -1,11 +1,6 @@
 export const formatUnreadMessageNumber = (num: number): string => {
-  if (num >= 1_000_000_000) {
-    return (num / 1_000_000_000).toFixed(1).replace(".", ",") + " Mrd"; // Milliarde
-  } else if (num >= 1_000_000) {
-    return (num / 1_000_000).toFixed(1).replace(".", ",") + " Mio"; // Million
-  } else if (num >= 1_000) {
-    return (num / 1_000).toFixed(1).replace(".", ",") + " Tsd"; // Tausend
-  } else {
-    return num.toString();
+  if (num >= 100) {
+    return "99+";
   }
+  return num.toString();
 };
