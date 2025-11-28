151c1aec33d31ed86b2393232b3b7d0b7db53638
milyasbpa <basilyas.2015@gmail.com>
2025-11-28 10:20:21 +0700

    fix: account

--- src/features/account/detail/container/Account.support.container.tsx
+++ src/features/account/detail/container/Account.support.container.tsx
@@ -1,18 +1,16 @@
 import * as React from "react";
 import clsx from "clsx";
 import { InformationAccountSupport } from "../fragments/information";
-import { LogoutAccountSupport } from "../fragments/logout/Logout.account.support";
 
 export const AccountSupportContainer = () => {
   return (
     <div
       className={clsx(
-        "grid grid-cols-1 place-content-start place-items-start gap-[1.5rem]",
+        "grid grid-cols-2 place-content-start place-items-start gap-[1.5rem]",
         "w-full"
       )}
     >
       <InformationAccountSupport />
-      <LogoutAccountSupport />
     </div>
   );
 };
