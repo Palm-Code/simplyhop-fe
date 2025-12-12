cdcbbb3c734c0abd4f61f8dafbeaa20c1456732e
milyasbpa <basilyas.2015@gmail.com>
2025-12-12 10:34:54 +0700

    fix: nextjs warning

--- next.config.js
+++ next.config.js
@@ -8,7 +8,9 @@ const withPWA = require("@ducanh2912/next-pwa").default({
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  turbopack: {},
+  turbopack: {
+    root: process.cwd(),
+  },
   images: {
     remotePatterns: [
       {
