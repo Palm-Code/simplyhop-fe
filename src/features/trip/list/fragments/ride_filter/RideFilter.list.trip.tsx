5e396615eb7b01bbe0a05086c386fa7a093a3e4a
milyasbpa <basilyas.2015@gmail.com>
2025-12-15 14:19:41 +0700

    fix: hide filter

--- src/features/trip/list/fragments/ride_filter/RideFilter.list.trip.tsx
+++ src/features/trip/list/fragments/ride_filter/RideFilter.list.trip.tsx
@@ -4,15 +4,25 @@ import clsx from "clsx";
 import { getDictionaries } from "../../i18n";
 import { useSearchParams, usePathname } from "next/navigation";
 import { TripFilterTabButton } from "@/core/components/trip_filter_tab_button";
+import { UserContext } from "@/core/modules/app/context";
 
 export const RideFilterListTrip = () => {
   const searchParams = useSearchParams();
   const pathname = usePathname();
   const dictionaries = getDictionaries();
+  const { state: userState } = React.useContext(UserContext);
+  const isSuperAdmin = userState.profile?.is_super_admin;
+  const isOrganizationAdmin =
+    userState.profile?.role === "admin" && !userState.profile.is_super_admin;
 
   const rideStatus = searchParams.get("ride-status");
   const type = searchParams.get("type");
 
+  const items = !userState.profile
+    ? []
+    : isSuperAdmin || isOrganizationAdmin
+    ? dictionaries.filter.ride.items.filter((item) => item.id !== "archive")
+    : dictionaries.filter.ride.items;
   return (
     <div
       className={clsx(
@@ -21,7 +31,7 @@ export const RideFilterListTrip = () => {
         "overflow-auto"
       )}
     >
-      {dictionaries.filter.ride.items.map((item, index) => {
+      {items.map((item, index) => {
         let params = index === 0 ? undefined : new URLSearchParams();
 
         if (index !== 0) {
