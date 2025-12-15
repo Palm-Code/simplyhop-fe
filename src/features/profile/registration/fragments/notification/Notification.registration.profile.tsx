460c6f8ca0988bc4b2b5e32d331314097d4e0a1b
milyasbpa <basilyas.2015@gmail.com>
2025-12-15 08:23:23 +0700

    fix: update color

--- src/features/profile/registration/fragments/notification/Notification.registration.profile.tsx
+++ src/features/profile/registration/fragments/notification/Notification.registration.profile.tsx
@@ -71,18 +71,23 @@ export const NotificationRegistrationProfile = () => {
               "flex items-center justify-center",
               "w-[120px] h-[120px]",
               "rounded-[50%]",
-              "bg-[#EFF9EC]"
+              "bg-[#EFF9EC] dark:bg-[#232323]"
             )}
           >
             <SVGIcon
               name="ContrastCheckMark"
-              className={clsx("w-[5rem] h-[5rem]", "text-[#33CC33]")}
+              className={clsx(
+                "w-[5rem] h-[5rem]",
+                "text-[#249124] dark:text-[#33CC33]"
+              )}
             />
           </div>
         </div>
 
         <h1
-          className={clsx("text-[1.5rem] text-[black] font-bold text-center")}
+          className={clsx(
+            "text-[1.5rem] text-[black] dark:text-white font-bold text-center"
+          )}
         >
           {dictionaries.notification.title}
         </h1>
