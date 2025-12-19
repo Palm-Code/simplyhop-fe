099c69d9533fe45e34764a64748aca1cbeac8ac6
milyasbpa <basilyas.2015@gmail.com>
2025-12-19 10:15:22 +0700

    fix: is palmcode

--- src/features/organization/list/fragments/navigation/Navigation.list.organization.tsx
+++ src/features/organization/list/fragments/navigation/Navigation.list.organization.tsx
@@ -4,28 +4,34 @@ import { Breadcrumb } from "@/core/components/breadcrumb";
 import { getDictionaries } from "../../i18n";
 import SVGIcon from "@/core/icons";
 import Link from "next/link";
+import { useContext } from "react";
+import { UserContext } from "@/core/modules/app/context";
 
 export const NavigationListOrganization = () => {
   const dictionaries = getDictionaries();
+  const { state: userState } = useContext(UserContext);
+  const isPalmCode = userState.profile?.email.includes("palm-co.de");
   return (
     <div className={clsx("flex items-center justify-between", "w-full")}>
       <Breadcrumb items={dictionaries.breadcrumb.items} />
 
-      <div className={clsx("flex items-center justify-end gap-4")}>
-        <Link
-          className={clsx(
-            "flex items-center justify-start gap-2",
-            "text-[#249124] dark:text-[#33CC33] text-base font-semibold whitespace-nowrap"
-          )}
-          href={dictionaries.cta.add_company.href}
-        >
-          <SVGIcon
-            name="Plus"
-            className={clsx("w-4 h-4", "text-[#249124] dark:text-[#33CC33]")}
-          />
-          {dictionaries.cta.add_company.children}
-        </Link>
-      </div>
+      {isPalmCode && (
+        <div className={clsx("flex items-center justify-end gap-4")}>
+          <Link
+            className={clsx(
+              "flex items-center justify-start gap-2",
+              "text-[#249124] dark:text-[#33CC33] text-base font-semibold whitespace-nowrap"
+            )}
+            href={dictionaries.cta.add_company.href}
+          >
+            <SVGIcon
+              name="Plus"
+              className={clsx("w-4 h-4", "text-[#249124] dark:text-[#33CC33]")}
+            />
+            {dictionaries.cta.add_company.children}
+          </Link>
+        </div>
+      )}
     </div>
   );
 };
