b598e52240180fb82a24d125f88314a902bb4392
milyasbpa <basilyas.2015@gmail.com>
2026-01-05 15:39:28 +0700

    fix: user detail driver

--- src/core/models/data/user_profile.ts
+++ src/core/models/data/user_profile.ts
@@ -1,7 +1,7 @@
 export interface UserProfile {
   id: number; //1;
   mobile_is_show: boolean; //false;
-  bio: string; //"Neque rerum itaque eius saepe.";
+  bio: string | null; //"Neque rerum itaque eius saepe.";
   information: string; //"Aut eum asperiores fuga qui. Laudantium magni animi magni assumenda consectetur omnis voluptatem. Veniam voluptas esse quibusdam et eius.";
   deleted_at: string | null;
   created_at: string; //"2025-04-22T03:44:27.000000Z";
