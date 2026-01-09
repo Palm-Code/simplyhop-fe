6eb7c55da5ff75a50ab82df03488b11e99af1185
milyasbpa <basilyas.2015@gmail.com>
2026-01-09 12:04:41 +0700

    fix: navigation breadcrumb

--- src/features/driver/organization/fragments/navigation/Navigation.organization.driver.tsx
+++ src/features/driver/organization/fragments/navigation/Navigation.organization.driver.tsx
@@ -4,13 +4,19 @@ import clsx from "clsx";
 import { Breadcrumb } from "@/core/components/breadcrumb";
 import { getDictionaries } from "../../i18n";
 import { OrganizationDriverContext } from "../../context";
+import { useParams } from "next/navigation";
 
 export const NavigationOrganizationDriver = () => {
   const dictionaries = getDictionaries();
   const { state } = React.useContext(OrganizationDriverContext);
+  const { organization_id } = useParams();
   const items = dictionaries.breadcrumb.items.map((item) => {
     return {
       ...item,
+      href: item.href.replaceAll(
+        "{{organization_id}}",
+        String(organization_id ?? "0")
+      ),
       label: item.label.replaceAll(
         "{{organization_name}}",
         state.profile.data?.organization.name ?? ""
