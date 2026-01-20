b2d9c494d91dfae0aa61f239a2b5405a8331a88a
milyasbpa <basilyas.2015@gmail.com>
2026-01-20 10:42:59 +0700

    fix: faq v1

--- src/features/help_center/faq/fragments/tab/Tab.faq.help_center.tsx
+++ src/features/help_center/faq/fragments/tab/Tab.faq.help_center.tsx
@@ -5,52 +5,19 @@ import { TabButton } from "@/core/components/tab_button";
 
 export const TabFAQHelpCenter = () => {
   const dictionaries = getDictionaries();
-  const [activeTab, setActiveTab] = React.useState<{
-    id: string;
-    name: string;
-  } | null>(dictionaries.tab.items[0] ?? null);
 
-  const handleClickTabItem = (data: { id: string; name: string }) => {
-    setActiveTab(data);
-  };
-
-  const content =
-    dictionaries.faq.items.find((item) => item.tabId === activeTab?.id) ?? null;
+  const content = dictionaries.faq.items?.[0] ?? null;
   return (
     <div
       className={clsx(
         "grid grid-cols-1 place-content-start place-items-start gap-[1rem]",
-        "w-full"
+        "w-full",
       )}
     >
       <div
         className={clsx(
-          "grid grid-flow-col grid-cols-none lg:grid-cols-2 place-content-start place-items-start gap-[1rem]",
-          "w-full",
-          "overflow-auto",
-          "border-b border-b-[#A6A6A6]"
-        )}
-      >
-        {dictionaries.tab.items.map((tabItem, tabIndex) => (
-          <TabButton
-            key={tabIndex}
-            selected={activeTab?.id === tabItem.id}
-            variant={"horizontal"}
-            className={clsx(
-              "!text-center",
-              "!justify-center !justify-items-center"
-            )}
-            onClick={() => handleClickTabItem(tabItem)}
-          >
-            {tabItem.name}
-          </TabButton>
-        ))}
-      </div>
-
-      <div
-        className={clsx(
           "grid grid-cols-1 place-content-start place-items-start gap-[1.5rem]",
-          "w-full"
+          "w-full",
         )}
       >
         {content?.list.map((listItem, listIndex) => (
@@ -58,7 +25,7 @@ export const TabFAQHelpCenter = () => {
             key={listIndex}
             className={clsx(
               "grid grid-cols-1 place-content-start place-items-start gap-[0rem]",
-              "w-full"
+              "w-full",
             )}
           >
             <p className={clsx("text-[#232323] text-[1rem] font-semibold")}>
