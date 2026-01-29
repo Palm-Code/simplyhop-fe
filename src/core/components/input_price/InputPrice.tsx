95229ffe9f5ca07ea7e7dd06eb1b4b52fbd5661f
milyasbpa <basilyas.2015@gmail.com>
2026-01-29 11:17:23 +0700

    fix: input price

--- src/core/components/input_price/InputPrice.tsx
+++ src/core/components/input_price/InputPrice.tsx
@@ -5,12 +5,18 @@ export const InputPrice = forwardRef<
   HTMLInputElement,
   React.InputHTMLAttributes<HTMLInputElement>
 >((props, ref) => {
+  const handleWheel = (e: React.WheelEvent<HTMLInputElement>) => {
+    e.currentTarget.blur();
+  };
+
   return (
     <input
       ref={ref}
       {...props}
       type="number"
       autoComplete="off"
+      min="0"
+      onWheel={handleWheel}
       className={clsx(
         "h-full w-[228px]",
         "bg-[white] dark:bg-[#232323]",
